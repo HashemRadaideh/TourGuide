56c8b80924a1fca65cfcd8cde59d1f202c386a9c
HashemRadaideh <hashemalradaideh@gmail.com>
2023-06-03 20:19:16 +0300

    Refactored the tour page

--- src/main/java/com/TourGuide/controller/Tour.java
+++ src/main/java/com/TourGuide/controller/Tour.java
@@ -18,45 +18,67 @@ public class Tour extends HttpServlet {
         super();
     }
 
-protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
-        throws ServletException, IOException {
-    response.setContentType("text/html");
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
 
-    final var content = new StringBuilder();
+        final var content = new StringBuilder();
 
-    final var url = "jdbc:mysql://localhost:3306/";
-    final var db_username = "admin";
-    final var db_password = "password";
-    final var database = "TourGuide";
+        final var url = "jdbc:mysql://localhost:3306/";
+        final var db_username = "admin";
+        final var db_password = "password";
+        final var database = "TourGuide";
 
-    try {
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-        final var connection = DriverManager.getConnection(url + database, db_username, db_password);
+            final var connection = DriverManager.getConnection(url + database, db_username, db_password);
 
-        final var prompt = connection.createStatement();
+            final var statement = connection.createStatement();
 
-        final var query = "SELECT p.*, u.username FROM post p JOIN user u ON p.ownerid = u.id";
-        final var post = prompt.executeQuery(query);
+            final var query = "SELECT p.*, u.username, " +
+                    "COUNT(CASE WHEN p.status = 1 THEN 1 END) AS trafficViolations, " +
+                    "COUNT(CASE WHEN p.status = 2 THEN 1 END) AS stopViolations, " +
+                    "COUNT(CASE WHEN p.status = 3 THEN 1 END) AS jaywalkingViolations, " +
+                    "COUNT(CASE WHEN p.status = 4 THEN 1 END) AS litteringViolations " +
+                    "FROM post p JOIN user u ON p.ownerid = u.id GROUP BY p.countryname, p.cityname";
+            final var resultSet = statement.executeQuery(query);
 
-        while (post.next()) {
             content.append("<div class=\"block\">");
-            content.append("<h1>" + post.getString("username") + "</h1>"); // Display username
-            content.append("<p>" + post.getString("content") + "</p>");
-            content.append("<a href=\"/Report?postId=" + post.getInt("id") + "\" class=\"button\">Report</a>");
+            while (resultSet.next()) {
+                content.append("<h2>u/" + resultSet.getString("username") + "</h2>");
+                content.append("<p>" + resultSet.getString("content") + "</p>");
+
+                content.append(
+                        "<h1>" + resultSet.getString("cityname") + ", " + resultSet.getString("countryname") + "</h1>");
+
+                int litteringViolations = resultSet.getInt("littercount");
+                String cleanliness = (litteringViolations >= 10) ? "Dirty" : "Clean";
+                content.append("<p>Cleanliness: " + cleanliness + "</p>");
+
+                int trafficViolations = resultSet.getInt("trafficcount");
+                int stopViolations = resultSet.getInt("stopcount");
+                String safety = ((trafficViolations >= 10) || (stopViolations >= 10)) ? "Dangerous" : "Safe";
+                content.append("<p>Safety: " + safety + "</p>");
+
+                int jaywalkingViolations = resultSet.getInt("jaywalkcount");
+                String sanity = (jaywalkingViolations >= 10) ? "Insane" : "Sane";
+                content.append("<p>Sanity of Residents: " + sanity + "</p>");
+
+                content.append("<a href=\"/Report?postId=" + resultSet.getInt("id") + "\" class=\"button\">Report</a>");
+            }
             content.append("</div>");
+
+            request.setAttribute("content", content.toString());
+            request.getRequestDispatcher("tour.jsp").forward(request, response);
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            response.getWriter().println("Error: " + e.getMessage());
         }
-
-        request.setAttribute("content", content.toString());
-        request.getRequestDispatcher("tour.jsp").forward(request, response);
-
-        post.close();
-        prompt.close();
-        connection.close();
-    } catch (ClassNotFoundException | SQLException e) {
-        response.getWriter().println("Error: " + e.getMessage());
     }
-}
 
     protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
             throws ServletException, IOException {
--- src/main/sql/post.sql
+++ src/main/sql/post.sql
@@ -1,15 +1,95 @@
 CREATE TABLE post (
     id INT PRIMARY KEY AUTO_INCREMENT,
     ownerid INT,
+    countryname VARCHAR(255) NOT NULL,
+    cityname VARCHAR(255) NOT NULL,
+    status INT,
+    isclean INT,
+    residents INT,
     content VARCHAR(255) NOT NULL,
-    reportcount INT DEFAULT 0,
+    trafficcount INT DEFAULT 0,
+    stopcount INT DEFAULT 0,
+    jaywalkcount INT DEFAULT 0,
+    littercount INT DEFAULT 0,
     FOREIGN KEY (ownerid) REFERENCES user (id)
 );
 
--- Post table
-INSERT INTO post (ownerid, content, reportcount)
+INSERT INTO post (
+    ownerid,
+    countryname,
+    cityname,
+    status,
+    isclean,
+    residents,
+    content,
+    trafficcount,
+    stopcount,
+    jaywalkcount,
+    littercount
+)
 VALUES
-(1, 'Hello world!', 0),
-(2, 'Check out this cool photo.', 0),
-(3, 'Any recommendations for good restaurants?', 0),
-(4, 'Excited for the weekend!', 0);
+(
+    1,
+    'United States',
+    'New York',
+    1,
+    1,
+    1000,
+    'Lorem ipsum dolor sit amet',
+    10,
+    2,
+    5,
+    3
+),
+(
+    2,
+    'United Kingdom',
+    'London',
+    1,
+    0,
+    500,
+    'Consectetur adipiscing elit',
+    5,
+    1,
+    0,
+    1
+),
+(
+    3,
+    'Canada',
+    'Toronto',
+    0,
+    1,
+    2000,
+    'Nulla facilisi',
+    15,
+    3,
+    7,
+    2
+),
+(
+    4,
+    'Australia',
+    'Sydney',
+    1,
+    1,
+    800,
+    'Pellentesque habitant morbi',
+    8,
+    1,
+    3,
+    0
+),
+(
+    5,
+    'Germany',
+    'Berlin',
+    0,
+    0,
+    1500,
+    'Aenean nec magna euismod',
+    2,
+    0,
+    1,
+    1
+);
