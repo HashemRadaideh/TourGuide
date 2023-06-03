c03ef43e010e873104c31805c487738c9ea273a8
HashemRadaideh <hashemalradaideh@gmail.com>
2023-06-03 11:51:27 +0300

    Connected to MySQL database and now can display content from the database

--- src/main/java/com/TourGuide/controller/Tour.java
+++ src/main/java/com/TourGuide/controller/Tour.java
@@ -1,16 +1,14 @@
 package com.TourGuide.controller;
 
 import java.io.IOException;
-
-import com.TourGuide.database.Database;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-// import com.TourGuide.database.*;
 
 @WebServlet("/Tour")
 public class Tour extends HttpServlet {
@@ -24,24 +22,41 @@ public class Tour extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html");
 
-        var db = Database.getInstance();
-        var table = db.getTable("posts");
-
         var content = new StringBuilder();
 
-        for (int i = 0; i < 10; i++) {
-            String userId = "" + i;
-            String postId = "" + i;
-            content.append("<div class=\"block\">");
-            content.append("<h1>Hello, World!</h1>");
-            content.append("<a href=\"/Report?userId=" + userId /* get current user id */ +
-                    "&postId=" + postId /* get the post's id to report */ +
-                    "\" class=\"button\">Report</a>");
-            content.append("</div>");
-        }
+        var url = "jdbc:mysql://localhost:3306/";
+        var username = "admin";
+        var password = "password";
+        var database = "TourGuide";
 
-        request.setAttribute("content", content);
-        request.getRequestDispatcher("tour.jsp").forward(request, response);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            var connection = DriverManager.getConnection(url + database, username, password);
+
+            var prompt = connection.createStatement();
+
+            var query = "SELECT * FROM post";
+            var post = prompt.executeQuery(query);
+
+            while (post.next()) {
+                content.append("<div class=\"block\">");
+                content.append("<h1>" + post.getString("username") + "</h1>");
+                content.append("<p>" + post.getString("content") + "</p>");
+                content.append("<a href=\"/Report?postId=" + post.getInt("id") +
+                        "\" class=\"button\">Report</a>");
+                content.append("</div>");
+            }
+
+            request.setAttribute("content", content);
+            request.getRequestDispatcher("tour.jsp").forward(request, response);
+
+            post.close();
+            prompt.close();
+            connection.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            response.getWriter().println("Error: " + e.getMessage());
+        }
     }
 
     protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
